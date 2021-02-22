1623d3871e86e4b99ea1a57ac03d9cfacded050b
dany <stefanusdany12@gmail.com>
2021-02-22 11:02:28 +0700

    menambah val coba

--- app/src/main/java/com/example/catatan/MainActivity.kt
+++ app/src/main/java/com/example/catatan/MainActivity.kt
@@ -2,12 +2,12 @@ package com.example.catatan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        val coba = findViewById<TextView>(R.id.coba)
     }
 }
