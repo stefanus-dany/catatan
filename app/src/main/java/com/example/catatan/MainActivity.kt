7ac17e049ce15e096b0986efbf8dd62fff0b95d5
Stefanus Dany <59822877+stefanusdany12@users.noreply.github.com>
2021-02-22 10:59:47 +0700

    menghapus toast di mainactivity.kt

--- app/src/main/java/com/example/catatan/MainActivity.kt
+++ app/src/main/java/com/example/catatan/MainActivity.kt
@@ -2,14 +2,12 @@ package com.example.catatan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this, "Hi All", Toast.LENGTH_SHORT).show()
 
     }
 }
